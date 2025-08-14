84a6d20e5e4517ff90aa12aa56bb530f433e2ad9
raheelanwaar03 <raheelanwaar0316@gmail.com>
2025-08-15 01:11:09 +0500

    okay

--- routes/user.php
+++ routes/user.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('User/')->name('User.')->middleware('auth', 'user', 'status')->group(function () {
-
     Route::get('Dashboard', [UserDashboardController::class, 'index'])->name('Dashboard');
     Route::get('Widthraw/Amount', [UserDashboardController::class, 'widthraw'])->name('Widthraw.Amount');
     Route::post('Store/Widthraw', [UserDashboardController::class, 'storeWidthraw'])->name('Store.Widthraw');
