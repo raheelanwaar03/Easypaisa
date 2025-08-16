fb96228de72841a75afb0276e30d49381553c532
raheelanwaar03 <raheelanwaar0316@gmail.com>
2025-08-17 00:09:54 +0500

    number

--- database/migrations/2023_10_14_180322_create_buy_plans_table.php
+++ database/migrations/2023_10_14_180322_create_buy_plans_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('plan_name');
             $table->string('plan_investment');
             $table->string('name');
-            $table->string('number');
             $table->string('trxId');
             $table->string('status')->default('pending');
             $table->timestamps();
--- database/migrations/2023_10_16_215101_create_easy_paisa_mangements_table.php
+++ database/migrations/2023_10_16_215101_create_easy_paisa_mangements_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('easy_paisa_mangements', function (Blueprint $table) {
             $table->id();
             $table->string('easy_name');
+            $table->string('easy_num');
             $table->string('bank');
             $table->string('status');
             $table->timestamps();
